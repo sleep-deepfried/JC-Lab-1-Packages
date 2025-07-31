221397f64c4c82812b65650f2e1bf06150b1406f
sleep-deepfried <pulidoearljohn14@gmail.com>
2025-07-31 13:55:28 +0800

    Final Submission Laboratory: Java Packages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,20 @@
 package org.example;
 
+import org.example.packageone.ClassOne;
+import org.example.packagetwo.ClassTwo;
+import org.example.packagethree.ClassThree;
 
-// Import here manually
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
+        ClassOne classOne = new ClassOne();
+        ClassTwo classTwo = new ClassTwo();
+        ClassThree classThree = new ClassThree();
 
+        out.println(classOne.greet());
+        out.println(classTwo.greet());
+        out.println(classThree.greet());
 
     }
 }
